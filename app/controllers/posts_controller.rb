71b14f9547fa39d2b28176f4ebb49fd28c4d67f3
Ubuntu <ubuntu@ip-172-31-33-152.ap-northeast-1.compute.internal>
2023-06-02 08:26:13 +0000

    完成しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,18 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
   end
+
   def create
-    @post = Post.new(post_params)    # 実装は終わっていないことに注意!
+    @post = Post.new(post_params)
     if @post.save
       flash[:success] = "投稿しました"
       redirect_to("/")
@@ -15,12 +21,28 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
+  def edit
     @post = Post.find(params[:id])
-    debugger
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      lash[:success] = "編集しました"
+      redirect_to("/")
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    flash[:success] = "削除しました"
+    redirect_to("/")
+  end
+
   private
-   def post_params
+    def post_params
      params.require(:post).permit(:title, :start,:finish,:alltime,:memo )
-   end
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
-  validates :title,  presence: true
-  validates :start, presence: true
+  validates :title,{presence: true,length:{maximum:20}}
+  validates :memo,length:{maximum:500}
+  validates :start,presence: true
+  validates :finish, presence: true
+  validate  :start_end_check
+
+  def start_end_check
+    errors.add(:error, "終了日は、開始日以降の日付としてください。") unless
+    self.start <= self.finish
+  end
+
 end
